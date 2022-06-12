83cde5f279459be50c0dcee775b107288eff39ed
axelbriandip <axelbriandip.rg@gmail.com>
2022-06-12 19:47:36 -0300

    cerrar modales al hacer click en botones

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 let arrayStock = [
-    {id:'001', name:'hoodie roja', price:'25.00', img:'https://img.represent.com/uploads/97cc04c33fb623827703747d01496782.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'002', name:'Sudadera negra', price:'15.00', img:'https://img.represent.com/uploads/ad9b3e6580cf66d28cbd90314ecdd0a6.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'003', name:'camisa blanca', price:'20.00', img:'https://img.represent.com/uploads/a76857819caae151f62fb58e05621acd.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'004', name:'camisa roja', price:'30.00', img:'https://img.represent.com/uploads/e33ba02f02bf3f18ba5e0db1a424ba56.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'005', name:'hoodie blanca', price:'10.00', img:'https://img.represent.com/uploads/535666bf499a0c508ace882bb0420436.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'006', name:'sudadera negra', price:'25.00', img:'https://img.represent.com/uploads/ee4a165cfdce4d17714bf8a39211746b.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'007', name:'hoodie negra', price:'5.00',img:'https://img.represent.com/uploads/46ac9d39acbd33cee2aadd5b6c230dc0.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'008', name:'sudadera blanca', price:'8.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'009', name:'sudadera blanca', price:'22.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'0010', name:'sudadera blanca', price:'30.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'0011', name:'sudadera blanca', price:'25.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'0012', name:'sudadera blanca', price:'19.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'0013', name:'sudadera blanca', price:'8.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
-    {id:'0014', name:'sudadera blanca', price:'10.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null}
+    {id:'001', name:'hoodie', price:'25.00', img:'https://img.represent.com/uploads/97cc04c33fb623827703747d01496782.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'002', name:'sudadera', price:'15.00', img:'https://img.represent.com/uploads/ad9b3e6580cf66d28cbd90314ecdd0a6.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'003', name:'camisa', price:'20.00', img:'https://img.represent.com/uploads/a76857819caae151f62fb58e05621acd.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'004', name:'camisa', price:'30.00', img:'https://img.represent.com/uploads/e33ba02f02bf3f18ba5e0db1a424ba56.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'005', name:'hoodie', price:'10.00', img:'https://img.represent.com/uploads/535666bf499a0c508ace882bb0420436.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'006', name:'sudadera', price:'25.00', img:'https://img.represent.com/uploads/ee4a165cfdce4d17714bf8a39211746b.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'007', name:'hoodie', price:'5.00',img:'https://img.represent.com/uploads/46ac9d39acbd33cee2aadd5b6c230dc0.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'008', name:'sudadera', price:'8.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'009', name:'camisa', price:'22.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'0010', name:'hoodie', price:'30.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'0011', name:'sudadera', price:'25.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'0012', name:'hoodie', price:'19.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'0013', name:'camisa', price:'8.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null},
+    {id:'0014', name:'sudadera', price:'10.00', img:'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format', color: null, talle: null}
 ]
 
 const mostrarStock = (array) => {
@@ -63,7 +63,7 @@ const modalPersonalizado = (id) => {
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                    <button type="button" class="btn btn-primary" onclick="agregarCarrito(${arrayStock[i].id})">Añadir al carrito</button>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick="agregarCarrito(${arrayStock[i].id})">Añadir al carrito</button>
                 </div>
                 </div>
             </div>`;
@@ -133,7 +133,7 @@ const mostrarCarrito = (array) => {
 
         html3 =
         `<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary btn-finalizar-compra">Finalizar compra</button>`;
+        <button type="button" class="btn btn-primary btn-finalizar-compra" data-bs-dismiss="modal">Finalizar compra</button>`;
         contenedorModalFooter.innerHTML = html3;
     }else {
         html = "<span class='material-symbols-outlined'>close</span> El carrito aún no tiene productos..<br><br>";
